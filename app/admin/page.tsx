5a2d72d84ce6b58e6817045eaab69c1322e930b9
llng <langzzstorelaxy@gmail.com>
2026-02-22 21:22:11 +0700

    config v2

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,7 +1,34 @@
-
+"use client"
+import NavbarAdmin from "@/components/navbarAdmin";
+import {Card, CardHeader, CardBody, CardFooter, Image} from "@heroui/react";
 
 export default function AdminDashboard() {
   return (
-    <div>dfs</div>
+    <>
+      <NavbarAdmin />
+      <CardAdmin />
+    </>
+  )
+}
+
+const CardAdmin = () => {
+  return (
+    <>
+          <Card className="py-4 w-50">
+      <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
+        <p className="text-tiny uppercase font-bold">Daily Mix</p>
+        <small className="text-default-500">12 Tracks</small>
+        <h4 className="font-bold text-large">Frontend Radio</h4>
+      </CardHeader>
+      <CardBody className="overflow-visible py-2">
+        <Image
+          alt="Card background"
+          className="object-cover rounded-xl"
+          src="https://heroui.com/images/hero-card-complete.jpeg"
+          width={270}
+        />
+      </CardBody>
+    </Card>
+    </>
   )
 }
---
+++ app/providers.tsx
@@ -0,0 +1,13 @@
+// app/providers.tsx
+'use client'
+
+import {HeroUIProvider} from '@heroui/react'
+// import {ThemeProvider as NextThemesProvider} from "next-themes";
+
+export function Providers({children}: { children: React.ReactNode }) {
+  return (
+    <HeroUIProvider>
+        {children}
+    </HeroUIProvider>
+  )
+}
